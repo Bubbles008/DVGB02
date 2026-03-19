296370a3b2ff6eb208d5400a3ed848b9643e5c45
Mido <34779315+midobolt@users.noreply.github.com>
2026-03-19 12:48:57 +0100

    Update node.c

--- Lab3/node.c
+++ Lab3/node.c
@@ -30,7 +30,59 @@ void rtinit(struct distance_table *table, int node) {
 	printdt(table, node);
 }
 
+
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-	printdt(table, node);
+
+    int changeDetected = 0;
+    int src = pkt->sourceid;
+
+
+    int costToSrc = table->costs[src][node];
+
+
+    for(int i = 0; i < 4; i++)  // i = destination
+    {
+        int newcost = costToSrc + pkt->mincost[i];
+
+        if(newcost < table->costs[i][src])
+        {
+            table->costs[i][src] = newcost;
+            changeDetected = 1;
+        }
+    }
+
+    if(changeDetected)
+    {
+        struct rtpkt outpkt;
+        outpkt.sourceid = node;
+
+        int best[4];
+
+        for(int d = 0; d < 4; d++)
+        {
+            int min = table->costs[d][0];
+
+            for(int v = 1; v < 4; v++)
+            {
+                if(table->costs[d][v] < min)
+                    min = table->costs[d][v];
+            }
+
+            best[d] = min;
+            outpkt.mincost[d] = min;
+        }
+
+        for(int n = 0; n < 4; n++)
+        {
+            if(is_neighbor(node, n))
+            {
+                outpkt.destid = n;
+                tolayer2(outpkt);
+            }
+        }
+    }
+
+    printdt(table, node);
 }
+
 
